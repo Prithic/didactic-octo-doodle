0ee61b4a68a1aff8664e4fc4bf5689bc4b773657
Prithic P <76239824+Prithic@users.noreply.github.com>
2025-12-07 13:28:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,14 @@
 // Create web server
 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require('http');const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello, World!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});const http = require('http');    
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
